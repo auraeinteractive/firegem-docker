927fd42828dd32fe720b25c5ce50f17e32c17ab2
Hogne Titlestad <ht@friendup.cloud>
2024-05-29 22:27:52 +0200

    Tweak to be clean.

--- assets/init-system.sh
+++ assets/init-system.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
-cp -R /tmp/Firegem/admin /var/www/html/
-cp -R /tmp/Firegem/lib /var/www/html/
-cp -R /tmp/Firegem/web /var/www/html/
-cp -R /tmp/Firegem/admin/admin.php /var/www/html/
-cp -R /tmp/Firegem/lib/htaccess /var/www/html/.htaccess
-cp -R /tmp/Firegem/web/index.php /var/www/html/
-rm -fr /tmp/Firegem
+# Function to manage Firegem from official repo
+require_firebase() {
+	bash requirements.sh
 
-mkdir -p /var/www/upload
-mkdir -p /var/www/upload/images-master
-mkdir -p /var/www/upload/images-cache
-mkdir -p /var/www/upload/templates
+	cp -R /tmp/Firegem/admin /var/www/html/
+	cp -R /tmp/Firegem/lib /var/www/html/
+	cp -R /tmp/Firegem/web /var/www/html/
+	cp -R /tmp/Firegem/admin/admin.php /var/www/html/
+	cp -R /tmp/Firegem/lib/htaccess /var/www/html/.htaccess
+	cp -R /tmp/Firegem/web/index.php /var/www/html/
+	rm -fr /tmp/Firegem
 
+	mkdir -p /var/www/upload
+	mkdir -p /var/www/upload/images-master
+	mkdir -p /var/www/upload/images-cache
+	mkdir -p /var/www/upload/templates
+}
+
+# Function to create database if it does not exist
+create_database() {
+	echo "Creating database if it does not exist..."
+	mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
+		CREATE DATABASE IF NOT EXISTS firecore;
+EOSQL
+	mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
+		CREATE DATABASE IF NOT EXISTS firegem;
+EOSQL
+	echo "Databases created."
+}
+
+
+require_firebase;
+
+create_database;
--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/autocaret/Firegem/
-cd Firegem && git checkout release/2.5.0 && cd ..
+if [ ! -d "./Firegem" ]; then
+	git clone https://github.com/autocaret/Firegem/
+	cd Firegem && git checkout release/2.5.0 && cd ..
+else
+	echo "Requirements satisfied."
+fi
+
